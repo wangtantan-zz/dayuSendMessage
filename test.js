055d85ee710fee526127c1c84142e8a3a61ff2c0
tantan <wangtantan09@163.com>
2016-09-29 18:37:44 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies.
+ */
+
+TopClient = require('./topClient').TopClient;
+
+const client = new TopClient({
+  appkey: '23469046',
+  appsecret: 'd66e6da2de042055140a4c22ca297251',
+  REST_URL: 'http://gw.api.taobao.com/router/rest',
+});
+
+sendMessage("15821788176", "涛哥侬好！", console.log);
+
+function sendMessage(mobile, content, cb) {
+  console.log({
+    extend   : '',
+    sms_type : 'normal',
+    sms_free_sign_name : '潭老师',
+    sms_param : `{"num":"${content}"}`,
+    rec_num : mobile,
+    sms_template_code : "SMS_16730374"
+  });
+
+  client.execute('alibaba.aliqin.fc.sms.num.send', {
+    extend   : '',
+    sms_type : 'normal',
+    sms_free_sign_name : '潭老师',
+    sms_param : `{"num":"${content}"}`,
+    rec_num : mobile,
+    sms_template_code : "SMS_16730374"
+  }, (err, ret) => {
+    cb(err, ret);
+  });
+}
